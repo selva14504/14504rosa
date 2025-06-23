7bb1337e046d72ab55c4c9cca404d94a5090219f
selva14504 <rselvakumarr2008@gmail.com>
2025-06-23 12:40:39 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f"Hello from ROSA Assessment WebApp! ENV: {os.environ.get('APP_ENV')}"
+    return f"Hello from Selva ROSA Assessment! ENV: {os.environ.get('APP_ENV')}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
